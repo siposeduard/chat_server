c5c82450f59f45ea19d1c7cd8156dc680eb6d9ba
Giusto <sipos.eduard1@gmail.com>
2021-02-03 20:16:53 +0200

    chat conversation

---
+++ lib/chat_server/chat/conversation.ex
@@ -0,0 +1,22 @@
+defmodule ChatServer.Chat.Conversation do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ChatServer.Chat.{ConversationMember, Message}
+
+  schema "chat_conversations" do
+    field :title, :string
+
+    has_many :conversation_members, ConversationMember
+    has_many :messages, Message
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(conversation, attrs) do
+    conversation
+    |> cast(attrs, [:title])
+    |> validate_required([:title])
+  end
+end
---
+++ lib/chat_server/chat/seen_message.ex
@@ -0,0 +1,21 @@
+defmodule ChatServer.Chat.SeenMessage do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ChatServer.Auth.User
+  alias ChatServer.Chat.Message
+
+  schema "chat_seen_messages" do
+    belongs_to :user, User
+    belongs_to :message, Message
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(seen_message, attrs) do
+    seen_message
+    |> cast(attrs, [:user_id, :message_id])
+    |> validate_required([:user_id, :message_id])
+  end
+end
